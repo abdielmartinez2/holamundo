09f03ef99b446fbddc71ea29f3f9bb03168fe496
abdielmartinez2 <abdiel.martinez2@utp.ac.pa>
2022-08-21 12:06:04 -0500

    Modificación de hola mundo

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
     <div class="container-sm">
         <?php  for ($i=1;$i<=7; $i++ )
-            echo "<$iHola Mundo</h$i>";
+            echo "<Hola Mundo</h$i>";
         ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
